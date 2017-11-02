555fe091f96d0280b621fb45e0c2712d530cbcf1
Ikey Doherty <ikey@solus-project.com>
2017-11-02 22:29:51 +0000

    Add initial dummy binary that does nothing
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/main.c
@@ -0,0 +1,35 @@
+/*
+ * This file is part of qol-assist.
+ *
+ * Copyright © 2017 Solus Project
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define __qol_unused__ __attribute__((unused))
+
+int main(__qol_unused__ int argc, __qol_unused__ char **argv)
+{
+        fprintf(stderr, "Not yet implemented\n");
+
+        return EXIT_FAILURE;
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
